2e7cd4a72324fba8eeb7d43cb0b6ac00698cde71
myOmikron <accounts@omikron.dev>
2021-03-17 19:00:11 +0100

    Set few default settings

--- bbb_frontend/bbb_frontend/settings.py
+++ bbb_frontend/bbb_frontend/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '30a(y#0ov2i173+8)uwb1qixg*bu#(cjo*^bvx8_p4nmo!lba0'
+SECRET_KEY = 'change_me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
 
 
 # Application definition
