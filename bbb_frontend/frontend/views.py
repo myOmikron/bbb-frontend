a4e55bf81c7f6900a3829a7768b98b421340e8be
myOmikron <accounts@omikron.dev>
2021-03-17 22:36:57 +0100

    Added additional details to errors

--- bbb_frontend/frontend/views.py
+++ bbb_frontend/frontend/views.py
@@ -11,11 +11,23 @@ from frontend.models import Channel
 class JoinView(View):
     def get(self, request, *args, **kwargs):
         if "user_name" not in request.GET:
-            return render(request, "info.html", {"info": "Missing parameter: user_name"}, status=400)
+            return render(
+                request, "info.html",
+                {"info": "Missing parameter: user_name", "status": "Bad request", "code": "400"},
+                status=400
+            )
         if "meeting_id" not in request.GET:
-            return render(request, "info.html", {"info": "Missing parameter: meeting_id"}, status=400)
+            return render(
+                request, "info.html",
+                {"info": "Missing parameter: meeting_id", "status": "Bad request", "code": "400"},
+                status=400
+            )
         if "checksum" not in request.GET:
-            return render(request, "info.html", {"info": "Missing parameter: checksum"}, status=400)
+            return render(
+                request, "info.html",
+                {"info": "Missing parameter: checksum", "status": "Bad request", "code": "400"},
+                status=400
+            )
         if not validate_checksum(
                 {
                     "checksum": request.GET["checksum"],
@@ -23,11 +35,19 @@ class JoinView(View):
                     "user_name": request.GET["user_name"]
                 }, settings.SHARED_SECRET, salt="join", time_delta=settings.SHARED_SECRET_TIME_DELTA
         ):
-            return render(request, "info.html", {"info": "You didn't pass the checksum test"}, status=403)
+            return render(
+                request, "info.html",
+                {"info": "You didn't pass the checksum test", "status": "Unauthorized", "code": "401"},
+                status=401
+            )
         try:
             Channel.objects.get(meeting_id=request.GET["meeting_id"])
         except Channel.DoesNotExist:
-            return render(request, "info.html", {"info": "Meeting does not exist on this server"})
+            return render(
+                request, "info.html",
+                {"info": "Meeting does not exist on this server", "status": "Not found", "code": "404"},
+                status=404
+            )
 
         user_name = request.GET["user_name"]
         meeting_id = request.GET["meeting_id"]
@@ -43,14 +63,30 @@ class WatchView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         if "checksum" not in request.session:
-            return render(request, "info.html", {"info": "Missing parameter: checksum"}, status=400)
+            return render(
+                request, "info.html",
+                {"info": "Missing parameter: checksum", "status": "Bad request", "code": "400"},
+                status=400
+            )
         if "user_name" not in request.session:
-            return render(request, "info.html", {"info": "Missing parameter: user_name"}, status=400)
+            return render(
+                request, "info.html",
+                {"info": "Missing parameter: user_name", "status": "Bad request", "code": "400"},
+                status=400
+            )
         if "session" not in request.GET:
-            return render(request, "info.html", {"info": "Missing parameter: meeting_id"}, status=400)
+            return render(
+                request, "info.html",
+                {"info": "Missing parameter: meeting_id", "status": "Bad request", "code": "400"},
+                status=400
+            )
         user_name = request.session["user_name"]
         meeting_id = request.GET["session"]
         tmp_checksum = hashlib.sha512(f"{user_name}{meeting_id}{settings.SHARED_SECRET}".encode("utf-8")).hexdigest()
         if request.session["checksum"] != tmp_checksum:
-            return render(request, "info.html", {"info": "You didn't pass the checksum check"}, status=403)
+            return render(
+                request, "info.html",
+                {"info": "You didn't pass the checksum check", "status": "Unauthorized", "code": "401"},
+                status=401
+            )
         return render(request, self.template_name)
