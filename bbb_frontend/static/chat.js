c2d8c2af12923f13d833afe45e3893583d77e0de
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-03-20 19:11:20 +0100

    Implemented websocket handling

---
+++ bbb_frontend/static/chat.js
@@ -0,0 +1,47 @@
+class Chat {
+    constructor() {
+    }
+
+    setupSocket() {
+        const url = window.location;
+        const protocol = url.protocol.replace("http", "ws");
+        const meeting_id = new URLSearchParams(url.search).get("meeting_id");
+
+        this.socket = new WebSocket(`${protocol}//${url.host}/watch/${meeting_id}`);
+
+        this.socket.onopen = () => {
+        };
+
+        this.socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if (data.type === "chat.message") {
+                this.onMessage(data);
+            } else {
+                console.error(`Incoming WebSocket json object is of unknown type: '${data.type}'`);
+            }
+        };
+
+        this.socket.onerror = (event) => {
+            console.error(event);
+        };
+
+        this.socket.onclose = () => {
+            setTimeout(this.setupSocket.bind(this), 1000);
+        };
+    }
+
+    sendMessage(message) {
+        this.socket.send(JSON.stringify({
+            type: "chat.message",
+            message,
+        }))
+    }
+
+    onMessage({type, user_name, message}) {
+        // TODO
+        console.log("" + user_name + " wrote:\n" + message)
+    }
+}
+
+const chat = new Chat();
+chat.open();
