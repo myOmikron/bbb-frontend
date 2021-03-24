60a3d51abc99ed4f072a633b43ab6cfef5e74868
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-03-25 00:48:24 +0100

    Changed layers backend to proper redis

--- bbb_frontend/bbb_frontend/settings.py
+++ bbb_frontend/bbb_frontend/settings.py
@@ -133,8 +133,11 @@ STATICFILES_DIRS = [
 # Channels
 ASGI_APPLICATION = "bbb_frontend.asgi.application"
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
