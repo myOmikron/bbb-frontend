64c645aa3c2988b4334e1352fa5567a5fd282d6a
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-03-25 03:40:59 +0100

    Moved sendMessage request into websocket consumer using httpx

--- bbb_frontend/chat/consumer.py
+++ bbb_frontend/chat/consumer.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 import logging
 import os
+from time import time
 
 from channels.consumer import SyncConsumer
 from channels.exceptions import InvalidChannelLayerError
@@ -10,6 +11,7 @@ from channels.db import database_sync_to_async
 from django.conf import settings
 from rc_protocol import get_checksum
 import requests
+import httpx
 
 from chat.models import Chat
 
@@ -19,24 +21,32 @@ class ChatConsumer(AsyncWebsocketConsumer):
     meeting_id: str
     user_name: str
 
+    logger = logging.getLogger(f"{__name__}.ChatConsumer")
+
     @database_sync_to_async
     def load_session(self):
         "Please don't be lazy" in self.scope["session"]._wrapped
 
+    @database_sync_to_async
+    def get_chat(self):
+        try:
+            return Chat.objects.get(self.meeting_id)
+        except Chat.DoesNotExist:
+            return None
+
     async def websocket_connect(self, message):
-        await self.load_session()
+        # Store relevant data
+        self.user_name = self.scope["session"]["user_name"]
+        self.meeting_id = self.scope["url_route"]["kwargs"]["meeting_id"]
 
         # Check for valid session data
+        await self.load_session()
         if "checksum" not in self.scope["session"]:
             await self.close(1008)
             return
         if "user_name" not in self.scope["session"]:
             await self.close(1008)
             return
-
-        # Store relevant data
-        self.user_name = self.scope["session"]["user_name"]
-        self.meeting_id = self.scope["url_route"]["kwargs"]["meeting_id"]
 
         # Check checksum
         tmp_checksum = hashlib.sha512(
@@ -67,8 +77,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         if data["type"] == "chat.message":
             data["user_name"] = self.user_name
-            await self.channel_layer.send("chatCallback", {"chat_id": self.meeting_id, **data})
+            # await self.channel_layer.send("chatCallback", {"chat_id": self.meeting_id, **data})
             await self.channel_layer.group_send(self.meeting_id, data)
+
+            chat = await self.get_chat()
+            if chat:
+                params = {
+                    "chat_id": self.meeting_id,
+                    "user_name": self.user_name,
+                    "message": data["message"]
+                }
+                params["checksum"] = get_checksum(params, chat.callback_secret, "sendMessage")
+
+                before = time()
+                async with httpx.AsyncClient() as client:
+                    await client.post(chat.callback_uri.rstrip("/") + "sendMessage", json=params)
+                self.logger.debug(f"Took {time() - before:.3f}ms to send request")
         else:
             raise ValueError(f"Incoming WebSocket json object is of unknown type: '{data['type']}'")
 
