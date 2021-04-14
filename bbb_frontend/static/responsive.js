7c3ba7b6358ced4457607c589d42537717023958
myOmikron <accounts@omikron.dev>
2021-04-14 17:29:41 +0200

    Address bar in mobile browser creates no scrollbar anymore

--- bbb_frontend/static/responsive.js
+++ bbb_frontend/static/responsive.js
@@ -27,3 +27,11 @@ function toggleChat() {
         }
     }
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
