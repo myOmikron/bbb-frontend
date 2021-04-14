f507ad85c2017bc2800b3408181a95671996cb8e
myOmikron <accounts@omikron.dev>
2021-04-14 17:14:33 +0200

    Made chat responsive for mobile use

--- bbb_frontend/static/responsive.js
+++ bbb_frontend/static/responsive.js
@@ -1,13 +1,29 @@
 function toggleChat() {
     let chat = document.getElementById("chat");
+    let video = document.getElementById("stream");
     let toggleButton = document.getElementById("toggleButton");
-    if (toggleButton.style.backgroundColor === "transparent") {
-        chat.style.display = "flex";
-        toggleButton.style.backgroundColor = "#0F70D7";
-        toggleButton.style.border = "2px solid #0F70D7";
+    let width = window.innerWidth;
+    if (width <= 680) {
+        if (chat.style.display === "flex") {
+            chat.style.display = "none";
+            video.style.display = "";
+            toggleButton.style.backgroundColor = "transparent";
+            toggleButton.style.border = "2px solid white";
+        } else {
+            chat.style.display = "flex";
+            video.style.display = "none";
+            toggleButton.style.backgroundColor = "#0F70D7";
+            toggleButton.style.border = "2px solid #0F70D7";
+        }
     } else {
-        chat.style.display = "none";
-        toggleButton.style.backgroundColor = "transparent";
-        toggleButton.style.border = "2px solid white";
+        if (chat.style.display === "none") {
+            chat.style.display = "flex";
+            toggleButton.style.backgroundColor = "#0F70D7";
+            toggleButton.style.border = "2px solid #0F70D7";
+        } else {
+            chat.style.display = "none";
+            toggleButton.style.backgroundColor = "transparent";
+            toggleButton.style.border = "2px solid white";
+        }
     }
 }
