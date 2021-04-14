f391332bba1c361f09a7c76333ae92246e80fb35
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-04-14 17:21:44 +0200

    Fixed routing

--- bbb_frontend/bbb_frontend/asgi.py
+++ bbb_frontend/bbb_frontend/asgi.py
@@ -3,13 +3,12 @@ import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-from chat.routing import websocket, channel
+from chat.routing import websocket
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bbb_frontend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": websocket,
-    "channel": channel,
 })
 
--- bbb_frontend/chat/routing.py
+++ bbb_frontend/chat/routing.py
@@ -2,14 +2,10 @@ from channels.routing import URLRouter, ChannelNameRouter
 from channels.sessions import SessionMiddlewareStack
 from django.urls import re_path
 
-from chat.consumer import ChatConsumer, ChatCallbackConsumer
+from chat.consumer import ChatConsumer
 
 websocket = SessionMiddlewareStack(
     URLRouter([
         re_path(r"watch/(?P<meeting_id>.+)", ChatConsumer.as_asgi()),
     ])
 )
-
-channel = ChannelNameRouter({
-    "chatCallback": ChatCallbackConsumer.as_asgi(),
-})
