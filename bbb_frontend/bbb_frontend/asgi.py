ece3faa5f721b8ae378f8e2603d809e09692b958
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-06-16 21:11:36 +0200

    Added uvicorn

--- bbb_frontend/bbb_frontend/asgi.py
+++ bbb_frontend/bbb_frontend/asgi.py
@@ -1,14 +1,15 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
-
-from frontend.routing import websocket
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bbb_frontend.settings')
 
+from django.core.asgi import get_asgi_application
+
+http = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter
+from frontend.routing import websocket
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": http,
     "websocket": websocket,
 })
